3b7714f8b6a0bead9865f4bf012656bcfb9b90ac
Tom Denham <tom@tomdee.co.uk>
2017-04-13 14:52:36 -0700

    WIP

--- backend/vxlan/vxlan.go
+++ backend/vxlan/vxlan.go
@@ -14,6 +14,42 @@
 
 package vxlan
 
+import "fmt"
+
+// Some design notes and history:
+// VXLAN encapsulates L2 packets (though flannel is L3 only so don't expect to be able to send L2 packets across hosts)
+// The first versions of vxlan for flannel registed the flannel daemon as a handler for both "L2" and "L3" misses
+// - When a container send a packet to a new IP address on the flannel network this generates an L2 miss (i.e. an ARP lookup)
+// - The flannel daemon knows which flannel host the packet is destined for so it can supply the VTEP MAC to use
+// - The packet can then be encapsulated but the host needs to know where to send it. This creates another callout to the
+// flannel daemon to get the public IP that should be used for that VTEP (this gets called an L3 miss). The L2/L3 miss
+// hooks are registered when the vxlan device is created. At the same time a device route is created to the whole
+// flannel network so that non-local traffic is sent over the vxlan device.
+//
+// In this screme, on each host there is
+//  - 1 route (for the configured network out the vxlan device)
+//  - One arp entry for each ??? container that we're talking to
+//  - One FDB entry for each ??? host?
+//
+// The second version of flannel vxlan removed the need for the L3MISS callout. When a new remote host is found (either
+// during startup or when it's created), flannel simply adds the required entries so that no further lookup/callout is required.
+//
+//
+// The latest version of version removes the need for the L2MISS too, which means that the flannel deamon is not
+// listening for any netlink messages anymore which improves reliablility (no worries about things timing out if
+// flannel crashes or restarts) and simplifies upgrades.
+//
+// How it works:
+// For each remote host
+// 1) create an route dev route but with the right subnet and next hop
+// 2) Create an ARP entry for every IP owned by that host?
+	NOOOOO this is wrong.
+What do I create an ARP entry for?
+// 3) Create an FDB entry?
+//
+
+// 1 route per host, 1 arp entry and one FDB entry per host
+
 import (
 	"encoding/json"
 	"fmt"
